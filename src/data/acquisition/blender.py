2a90800862517b970b3b72055c1a46c3445fd2cb
Marc Bresson <marco.bresson@gmail.com>
2023-06-20 16:18:18 +0200

    feat: save depth layer using the viewer node

--- src/data/acquisition/blender.py
+++ src/data/acquisition/blender.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from numpy import deg2rad
 import pandas as pd
 import bpy
+import numpy as np
 from math import radians, sin, cos, log, atan, tan
 
 
@@ -68,108 +69,13 @@ def create_camera(
     bpy.context.scene.render.image_settings.file_format = 'JPEG'
 
 
-def render_equirectangle_streetview(
-    xyz: tuple[float, float, float],
-    computed_compass_angle: float,
-    layers_to_render: list[str],
-    savepath: Path
-):
-    """
-    Parameters
-    ----------
-    xyz : tuple[float, float, float]
-        in meters
-    computed_compass_angle : float
-        in degree
-    """
-    camera = bpy.context.scene.camera
-    camera.location = xyz
-    camera.rotation_euler = mapillary_to_euler(computed_compass_angle)
-
-    for layer_name in layers_to_render:
-        select_pass(layer_name)
-
-        savepath_layer = savepath.with_stem(f"{savepath.stem}_{layer_name}").with_suffix(".jpg")
-        bpy.context.scene.render.filepath = str(savepath_layer)
-
-        bpy.ops.render.render(write_still=True)
-
-
-def init_node_tree():
-    bpy.context.scene.use_nodes = True
-
-    tree = bpy.context.scene.node_tree
-
-    rl_node = tree.nodes.new("CompositorNodeRLayers")
-    rl_node.location = (0, 0)
-    rl_node.name = "RenderLayers"
-    composite_node = tree.nodes.new("CompositorNodeComposite")
-    composite_node.location = (400, 0)
-    composite_node.name = "Composite"
-
-
-def select_pass(pass_name: str):
-    tree = bpy.context.scene.node_tree
-
-    rl_node = tree.nodes.get("RenderLayers")
-    composite_node = tree.nodes.get("Composite")
-
-    render_pass = rl_node.outputs[pass_name]
-    tree.links.new(render_pass, composite_node.inputs[0])
-
-
-def clean_nodes():
-    tree = bpy.context.scene.node_tree
-
-    for node in tree.nodes:
-        tree.nodes.remove(node)
-
-
-# def mapillary_to_euler(mapillary_rotation: tuple[float, float, float]) -> tuple[float, float, float]:
-#     roll, pitch, yaw = mapillary_rotation
-#     euler_rotation = mathutils.Euler((pitch, yaw, roll), 'XYZ')
-
-#     return euler_rotation
-
-
-# def mapillary_to_euler(mapillary_rotation: tuple[float, float, float]) -> tuple[float, float, float]:
-#     length = np.linalg.norm(mapillary_rotation)
-#     euler_rotation = Matrix.Rotation(length, 3, mapillary_rotation).to_euler("XYZ")
-
-#     return euler_rotation
-
-
 def mapillary_to_euler(compass_orientation: float) -> tuple[float, float, float]:
     euler_rotation = [90, 0, -compass_orientation]
 
     return deg2rad(euler_rotation)
 
 
-# render_pass_names = ["Depth", "Normal", "DiffCol"]
-
-# def main():
-#     clean_nodes()
-#     init_node_tree(layers_to_render)
-#     create_camera()
-
-#     dataset_path = Path(r"C:\Users\marco\Documents\Cours\Individual Research Project - IRP\code\data\dataset.arrow")
-#     savedir = Path(r"C:\Users\marco\Documents\Cours\Individual Research Project - IRP\code\data\blender")
-
-#     scene = bpy.context.scene
-#     projection = TransverseMercator(lat=scene["lat"], lon=scene["lon"])
-
-#     for i, view_data in enumerate(read_data(dataset_path)):
-#         image_id, lon, lat, alt, computed_compass_angle, rot_x, rot_y, rot_z = view_data
-
-#         x, y = projection.from_geographic(lat, lon)
-#         z = alt + 0.3
-
-#         savepath: Path = savedir / str(image_id)
-
-#         render_equirectangle_streetview((x, y, z), computed_compass_angle, layers_to_render, savepath)
-
-
-def create_nodes(tmp_dir: Path, render_pass_names: list[str]) -> list[str]:
+def create_nodes(tmp_dir: Path, render_pass_names: list[str], render_pass_options: list[dict]) -> list[str]:
     """
     create the node tree to save each pass in a different folder.
 
@@ -195,15 +101,24 @@ def create_nodes(tmp_dir: Path, render_pass_names: list[str]) -> list[str]:
     save_dirs = [""] * len(render_pass_names)
 
     for i, render_pass_name in enumerate(render_pass_names):
-        fileoutput_node = tree.nodes.new("CompositorNodeOutputFile")
-        fileoutput_node.location = (400, -120 * i)
-        fileoutput_node.name = render_pass_name
-
         save_dirs[i] = tmp_dir / render_pass_name
-        fileoutput_node.base_path = str(save_dirs[i])
 
         render_pass = renderlayers_node.outputs[render_pass_name]
-        tree.links.new(render_pass, fileoutput_node.inputs[0])
+
+        if "use_viewer" in render_pass_options[i]:
+            viewer_node = tree.nodes.new("CompositorNodeViewer")
+            output_node = viewer_node
+        else:
+            for key, value in render_pass_options[i].items():
+                setattr(bpy.context.scene.render.image_settings, key, value)
+            fileoutput_node = tree.nodes.new("CompositorNodeOutputFile")
+            fileoutput_node.location = (400, -120 * i)
+            fileoutput_node.name = render_pass_name
+
+            fileoutput_node.base_path = str(save_dirs[i])
+            output_node = fileoutput_node
+
+        tree.links.new(render_pass, output_node.inputs[0])
 
     return save_dirs
 
@@ -230,19 +145,30 @@ def place_camera(
     camera.rotation_euler = mapillary_to_euler(computed_compass_angle)
 
 
-def move_render_passes(save_dirs: Path, output_dir: Path):
+def move_render_passes(save_dirs: list[Path], output_dir: Path):
     image_id = output_dir.stem
 
     for dir_ in save_dirs:
         pass_img_path: Path = list(dir_.glob("*.*"))[0]
         pass_name = pass_img_path.parent.stem
+        pass_ext = pass_img_path.suffix
 
-        new_filepath = output_dir.with_stem(f"{image_id}_{pass_name}").with_suffix(".jpg")
+        new_filepath = output_dir.with_stem(f"{image_id}_{pass_name}").with_suffix(pass_ext)
         pass_img_path.rename(new_filepath)
 
 
-def main2():
+def save_viewernode(save_dir: Path):
+    viewer = np.array(bpy.data.images["Viewer Node"].pixels)
+    viewer = viewer[0::4].reshape((1024, 2048))  # remove duplicated channels
+    viewer = viewer[::-1, :]  # flip horizontally
+    viewer = viewer * (viewer < 1.0e+10)  # remove the sky
+    viewer = viewer.astype(np.uint16)  # save as int (way lighter)
+    np.save(save_dir / "raw", viewer)
+
+
+def main():
     render_pass_names = ["Depth", "Normal", "DiffCol"]
+    render_pass_options = [{"use_viewer": True}, {"file_format": "PNG", "color_depth": "8", "color_mode": "RGB"}, {"file_format": "PNG", "color_depth": "8", "color_mode": "RGB"}]
 
     root_dir = Path(r"C:\Users\marco\Documents\Cours\Individual Research Project - IRP\code\data")
 
@@ -251,12 +177,17 @@ def main2():
     savedir = root_dir / "blender"
 
     create_camera()
-    save_dirs = create_nodes(tmp_dir, render_pass_names)
+    save_dirs = create_nodes(tmp_dir, render_pass_names, render_pass_options)
 
     scene = bpy.context.scene
     projection = TransverseMercator(lat=scene["lat"], lon=scene["lon"])
 
+    count = 0
     for streetview_data in read_data(dataset_path):
+        if count > 5:
+            break
+        count += 1
+
         image_id, lon, lat, alt, computed_compass_angle = streetview_data
 
         x, y = projection.from_geographic(lat, lon)
@@ -264,4 +195,7 @@ def main2():
 
         bpy.ops.render.render(write_still=True)
 
-        move_render_passes(save_dirs, savedir / image_id)
+        save_viewernode(save_dirs[0])
+        move_render_passes(save_dirs, savedir / str(image_id))
+
+main()
