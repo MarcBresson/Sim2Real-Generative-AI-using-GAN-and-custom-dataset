f2da9a9e92b5b3f3653502e6233050a5170abd53
Marc Bresson <marco.bresson@gmail.com>
2023-07-24 09:11:20 +0200

    perf: create truth or false tensor with direct methods

--- src/eval/gan_loss.py
+++ src/eval/gan_loss.py
@@ -3,11 +3,16 @@ from torch import Tensor
 from torch import nn
 
 
-class BCEWithLogitsLoss(nn.BCEWithLogitsLoss):
+class BCEWithLogitsLoss():
     """extension class to work with simplified inputs"""
-    def __call__(self, input: Tensor, is_target_real: bool) -> Tensor:
-        bool_as_float = [float(is_target_real)]
-        target = torch.tensor(bool_as_float, device=input.device).expand_as(input)
+    def __init__(self, weight: Tensor = None, size_average=None, reduce=None, reduction: str = 'mean', pos_weight: Tensor = None) -> None:
+        self.loss = nn.BCEWithLogitsLoss(weight, size_average, reduce, reduction, pos_weight)
 
-        loss = super().__call__(input, target)
-        return loss
+    def __call__(self, input: Tensor, is_target_real: bool) -> Tensor:
+        if is_target_real:
+            target = torch.ones_like(input)
+        else:
+            target = torch.zeros_like(input)
+
+        loss_value = self.loss(input, target)
+        return loss_value
