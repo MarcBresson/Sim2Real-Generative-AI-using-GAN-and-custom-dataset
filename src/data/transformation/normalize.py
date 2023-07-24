568d19ce69a71ae1dcd25145c4a4c1e24734ee1b
Marc Bresson <marco.bresson@gmail.com>
2023-07-24 09:14:58 +0200

    feat: add toNumpy transformation

--- src/data/transformation/normalize.py
+++ src/data/transformation/normalize.py
@@ -1,5 +1,7 @@
 from torch import Tensor
 
+import numpy as np
+
 from .batcher import batcher
 
 
@@ -15,13 +17,12 @@ class NormalizeChannels():
         return imgs
 
 
-class UnNormalize():
+class toNumpy():
     """
     extends torchvision.transforms.Resize to suits this project
     """
-    def __call__(self, imgs: dict[str, Tensor]) -> dict[str, Tensor]:
-        imgs = batcher(imgs)
-        imgs["streetview"] = imgs["streetview"] * 255
-        imgs["simulated"] = imgs["simulated"] * 255
-
-        return imgs
+    def __call__(self, imgs: Tensor) -> np.ndarray:
+        imgs = imgs.detach().cpu()
+        imgs_norm = (imgs * 255).permute(0, 2, 3, 1)
+        imgs_np = imgs_norm.numpy().astype(np.uint8)
+        return imgs_np
