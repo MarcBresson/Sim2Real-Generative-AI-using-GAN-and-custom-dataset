e999f2f2bfb379c24b14bc16e1cca74a9a7882f7
Marc Bresson <marco.bresson@gmail.com>
2023-07-27 09:16:31 +0200

    build: export toNumpy to namespace

--- src/data/transformation/__init__.py
+++ src/data/transformation/__init__.py
@@ -1,2 +1,3 @@
 from src.data.transformation.perspective_transform import RandomPerspective
 from src.data.transformation.resize_transform import Resize
+from src.data.transformation.normalize import NormalizeChannels, toNumpy
