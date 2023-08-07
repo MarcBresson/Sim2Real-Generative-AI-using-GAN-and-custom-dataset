617dcecd0a5ff6b00dfb265f20f28c78ded8212e
Marc Bresson <marco.bresson@gmail.com>
2023-08-07 09:49:43 +0100

    feat(transformation): add ability to transform single Tensor

--- src/data/transformation/batcher.py
+++ src/data/transformation/batcher.py
@@ -1,13 +1,78 @@
-import torch
+from typing import Union
+
+from torch import Tensor
 
 
-def batcher(input_: dict[str, torch.Tensor]) -> torch.Tensor:
+def batcher(input_: Union[dict[str, Tensor], Tensor]) -> Union[dict[str, Tensor], Tensor]:
     """
     in case individual images were provided, convert them to batches
     """
-    if len(input_["streetview"].shape) == 3:
-        input_["streetview"] = input_["streetview"].unsqueeze(0)
-    if len(input_["simulated"].shape) == 3:
-        input_["simulated"] = input_["simulated"].unsqueeze(0)
+    if isinstance(input_, dict):
+        if len(input_["streetview"].shape) == 3:
+            input_["streetview"] = input_["streetview"].unsqueeze(0)
+        if len(input_["simulated"].shape) == 3:
+            input_["simulated"] = input_["simulated"].unsqueeze(0)
+
+    elif isinstance(input_, Tensor):
+        if len(input_.shape) == 3:
+            input_ = input_.unsqueeze(0)
 
     return input_
+
+
+class Sample2Batch():
+    def __call__(self, input_: Union[dict[str, Tensor], Tensor]) -> Union[dict[str, Tensor], Tensor]:
+        """
+        transform a single dict sample to a dict batchted sample
+        transform a single sample to a batched sample
+
+        Parameters
+        ----------
+        input_ : Union[dict[str, Tensor], Tensor]
+            the single sample
+
+        Returns
+        -------
+        Union[dict[str, Tensor], Tensor]
+            the batched sample
+        """
+        if isinstance(input_, dict):
+            if len(input_["streetview"].shape) == 3:
+                input_["streetview"] = input_["streetview"].unsqueeze(0)
+            if len(input_["simulated"].shape) == 3:
+                input_["simulated"] = input_["simulated"].unsqueeze(0)
+
+        elif isinstance(input_, Tensor):
+            if len(input_.shape) == 3:
+                input_ = input_.unsqueeze(0)
+
+        return input_
+
+
+class Batch2Sample():
+    def __call__(self, input_: Union[dict[str, Tensor], Tensor]) -> Union[dict[str, Tensor], Tensor]:
+        """
+        transform a dict batched sample to a dict single sample if the batch has 1 element
+        transform a batched sample to a single sample if the batch has 1 element
+
+        Parameters
+        ----------
+        input_ : Union[dict[str, Tensor], Tensor]
+            batch, either in a dict or in a single Tensor.
+
+        Returns
+        -------
+        Union[dict[str, Tensor], Tensor]
+            single sample
+        """
+        if isinstance(input_, dict):
+            if len(input_["streetview"].shape) == 4 and input_["streetview"].shape[0] == 1:
+                input_["streetview"] = input_["streetview"][0]
+            if len(input_["simulated"].shape) == 4 and input_["streetview"].shape[0] == 1:
+                input_["simulated"] = input_["simulated"][0]
+
+        elif isinstance(input_, Tensor):
+            if len(input_.shape) == 4 and input_.shape[0] == 1:
+                input_ = input_[0]
+
+        return input_
---
+++ src/data/transformation/resize.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+import torch
+import torchvision
+from torch import Tensor
+
+torch.manual_seed(972000)
+
+
+class Resize():
+    """
+    extends torchvision.transforms.Resize to suits this project
+    """
+    def __init__(
+        self,
+        *args,
+        **kwargs
+    ):
+        self.transform = torchvision.transforms.Resize(*args, **kwargs)
+
+    def __call__(self, imgs: Union[dict[str, Tensor], Tensor]) -> Union[dict[str, Tensor], Tensor]:
+        if isinstance(imgs, dict):
+            imgs["streetview"] = self.transform(imgs["streetview"])
+            imgs["simulated"] = self.transform(imgs["simulated"])
+        elif isinstance(imgs, Tensor):
+            imgs = self.transform(imgs)
+
+        return imgs
