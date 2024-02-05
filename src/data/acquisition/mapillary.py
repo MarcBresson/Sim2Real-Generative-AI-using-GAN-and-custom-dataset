166e604c71cf670ab4b55e75e8dd36b0fa195601
Marc Bresson <marco.bresson@gmail.com>
2024-02-05 19:32:24 +0100

    REFACTOR: clean file and document functions

--- src/data/acquisition/mapillary.py
+++ src/data/acquisition/mapillary.py
@@ -26,14 +26,15 @@ def process_img_data(image_id: int, image_data: dict):
     data = [image_id]
     data.append(image_data["camera_type"])
     data.append(image_data["captured_at"])
-    data.append(image_data["computed_altitude"])
     data.append(image_data["computed_compass_angle"])
+
+    data.append(image_data["computed_altitude"])
     # lon, lat
     data.extend(image_data["computed_geometry"]["coordinates"])
     # rot z, rot y, rot x. See https://opensfm.org/docs/cam_coord_system.html
     data.extend(image_data["computed_rotation"])
 
-    data.extend(image_data["thumb_2048_url"])
+    data.append(image_data["thumb_2048_url"])
 
     return data
 
@@ -41,7 +42,7 @@ def process_img_data(image_id: int, image_data: dict):
 def download_image_data(
     data_set: list,
     image_id: int,
-    fields_to_dl: list[str],
+    fields_to_dl: set[str],
     headers: dict,
     image_dir: Path
 ):
@@ -58,71 +59,135 @@ def download_image_data(
         download_image(image_dir, image_id, image_url)
 
 
-def inside_bbox(lon_lat, west_south_east_north) -> bool:
+def is_inside_bbox(lon_lat, region_nesw) -> bool:
     """ensure lon and lat are inside the bounding box since tiles
     can expand beyond"""
     lon, lat = lon_lat
-    west, south, east, north = west_south_east_north
+    north, east, south, west = region_nesw
     return west < lon < east and south < lat < north
 
 
-def image_exists(image_id: int, dataset: pandas.DataFrame):
+def image_already_exists(image_id: int, dataset: pandas.DataFrame):
     """return True if the image is in the dataset"""
-    if dataset is None:
+    if dataset.empty:
         return False
     return image_id in dataset["image_id"].values
 
 
-def main(access_token, data_dir: Path, dataset: list):
-    fields = ["thumb_2048_url", "camera_type", "captured_at", "computed_altitude", "computed_compass_angle", "computed_geometry", "computed_rotation"]
+def handle_tile(
+    access_token: str,
+    image_dir: Path,
+    existing_dataset: pandas.DataFrame,
+    dataset: list,
+    tile: mercantile.Tile,
+    region_nesw: tuple[float, float, float, float],
+    fields: set[str]
+):
+    """
+    handles the download of images in a tile.
+
+    Parameters
+    ----------
+    access_token : str
+        Mapillary's access token.
+    image_dir : Path
+        the directory where to save the images.
+    existing_dataset : pandas.DataFrame
+        the already existing dataframe (can be an empty
+        dataframe if none existed beforehand).
+    dataset : list
+        the current dataset to grow with the new data.
+    tile : mercantile.Tile
+        the tile to download features in.
+    region_nesw: tuple[float, float, float, float]
+        the region to download street views in with North, Est,
+        South, and West coordinates.
+    fields : set[str]
+        the fields to download and save in the growing dataframe.
+    """
     headers = {"Authorization": f"OAuth {access_token}"}
 
+    tile_url = f'https://tiles.mapillary.com/maps/vtp/mly1_public/2/{tile.z}/{tile.x}/{tile.y}?access_token={access_token}'
+    response = requests.get(tile_url, headers=headers, timeout=20)
+    tile_data = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z, layer="image")
+
+    for feature in tile_data['features']:
+        feature_lon = feature['geometry']['coordinates'][0]
+        feature_lat = feature['geometry']['coordinates'][1]
+
+        image_id = feature['properties']['id']
+
+        if feature['properties']['is_pano'] is False:
+            continue
+
+        if image_already_exists(image_id, existing_dataset):
+            continue
+
+        if is_inside_bbox((feature_lon, feature_lat), region_nesw):
+            threading.Thread(
+                target=download_image_data,
+                args=(dataset, feature['properties']['id'], fields, headers, image_dir)
+            ).start()
+
+
+def main(
+    access_token: str,
+    data_dir: Path,
+    region_nesw: tuple[float, float, float, float]
+):
+    """
+    download mapillary's street views
+
+    Parameters
+    ----------
+    access_token : str
+        mapillary access token. See more at
+        https://www.mapillary.com/dashboard/developers
+    data_dir : Path
+        the data directory where the mapillary street views will be saved
+        in the mapillary folder.
+    region_nesw: tuple[float, float, float, float]
+        the region to download street views in with North, Est,
+        South, and West coordinates.
+    """
+    fields = set(["thumb_2048_url", "camera_type", "captured_at", "computed_altitude", "computed_compass_angle", "computed_geometry", "computed_rotation"])
+
     image_dir = data_dir / "mapillary"
-    annotations_file = data_dir / "annotations.arrow"
+    image_dir.mkdir(parents=True, exist_ok=True)
+    annotations_file = data_dir / "annotations.csv"
 
+    # if a dataset already exists, we load it to avoid redownloading the same
+    # street views and allow for step by step loading.
     if annotations_file.exists():
-        dataset_pd = pandas.read_feather(annotations_file)
+        existing_dataset = pandas.read_feather(annotations_file)
     else:
-        dataset_pd = None
+        existing_dataset = pandas.DataFrame()
 
+    # we store everything in an array to avoid growing a pd dataframe.
+    dataset = []
+
+    # this try statement is just to allow for the dataset to be saved in case
+    # of unknown error.
     try:
         total_tiles = sum(1 for _ in mercantile.tiles(west, south, east, north, 14))
 
-        tiles_bar = tqdm(
+        tiles_progress_bar = tqdm(
             mercantile.tiles(west, south, east, north, 14),
             total=total_tiles
         )
-        for tile in tiles_bar:
-            tiles_bar.set_postfix(tile_z=tile.z, tile_x=tile.x, tile_y=tile.y)
+        for tile in tiles_progress_bar:
+            # gives more info in the progress bar.
+            tiles_progress_bar.set_postfix(tile_z=tile.z, tile_x=tile.x, tile_y=tile.y)
 
-            tile_url = f'https://tiles.mapillary.com/maps/vtp/mly1_public/2/{tile.z}/{tile.x}/{tile.y}?access_token={access_token}'
-            response = requests.get(tile_url, headers=headers)
-            tile_data = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z, layer="image")
+            handle_tile(access_token, image_dir, existing_dataset, dataset, tile, region_nesw, fields)
 
-            for feature in tile_data['features']:
-                lon = feature['geometry']['coordinates'][0]
-                lat = feature['geometry']['coordinates'][1]
-
-                image_id = feature['properties']['id']
-
-                if feature['properties']['is_pano'] is False:
-                    continue
-
-                if image_exists(image_id, dataset_pd):
-                    continue
-
-                if inside_bbox((lon, lat), (west, south, east, north)):
-                    threading.Thread(
-                        target=download_image_data,
-                        args=(dataset, feature['properties']['id'], fields, headers, image_dir)
-                    ).start()
     finally:
         print(f"{len(dataset)} new samples")
         new_dataset_pd = pandas.DataFrame(dataset, columns=["image_id", "camera_type", "capture_at", "computed_altitude", "computed_compass_angle", "lon", "lat", "rot x", "rot y", "rot z", "thumb_2048_url"])
 
         if annotations_file.exists():
-            dataset_pd = pandas.concat([new_dataset_pd, dataset_pd], ignore_index=True)
+            existing_dataset = pandas.concat([new_dataset_pd, existing_dataset], ignore_index=True)
         else:
-            dataset_pd = new_dataset_pd
+            existing_dataset = new_dataset_pd
 
-        dataset_pd.to_feather(annotations_file)
+        existing_dataset.to_csv(annotations_file)
