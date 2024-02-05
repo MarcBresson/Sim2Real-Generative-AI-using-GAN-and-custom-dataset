c35a7a7138ef36f42ee98dabfa514c0348ce6307
Marc Bresson <marco.bresson@gmail.com>
2024-02-05 19:31:41 +0100

    FEAT: create dataset for inference. It uses only blender views as input

---
+++ src/data/inference_dataset.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+
+from src.data.utils import dir_to_img_ids, get_simulated_image
+
+
+class InferenceDataset(Dataset):
+    def __init__(
+        self,
+        view_folder: Path
+    ):
+        self.view_folder = view_folder
+        self.view_ids = dir_to_img_ids(view_folder)
+
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        view_id = self.view_ids[idx]
+
+        sample = get_simulated_image(self.view_folder, view_id, return_nbr_of_channels_per_pass=False)
+
+        return sample
