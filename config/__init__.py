05d78fa50b4ac9f4e4652664361567670717673b
Marc Bresson <marco.bresson@gmail.com>
2024-03-18 08:29:31 +0100

    ENH: convert a config object to a dict (non -recursively)

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,16 @@
+from typing import Any
+
+from pydantic import BaseModel
+
 from config.base_models import *  # noqa: F403
+
+
+def get_config_to_dict(conf: BaseModel | None) -> dict[str, Any]:
+    """convert a configuration to a dictionnary ready to be unpacked."""
+    if conf is None:
+        return {}
+
+    if isinstance(conf, BaseModel):
+        return dict(conf)
+
+    return conf
